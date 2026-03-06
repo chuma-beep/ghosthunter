91573ea90d56d6c9d6e414a123fd5276e2100552
chuma-beep <wisboynelson123@gmail.com>
2026-03-06 11:25:33 +0100

    added structs for sprites

--- internal/engine/sprite.go
+++ internal/engine/sprite.go
@@ -16,6 +16,23 @@ type AmmoPickup struct {
     Active bool
 }
 
+type Entity struct{
+	X  float64
+	Y  float64
+	VX float64
+	VY float64
+	FadeTimer int 
+	Dead bool 
+	Type EntityType 
+	Health int 
+	Speed  float64
+	Damage int 
+}
 
+type AmmoPickup struct {
+	X float64
+	Y float64
+	Active bool 
+}
 
 
